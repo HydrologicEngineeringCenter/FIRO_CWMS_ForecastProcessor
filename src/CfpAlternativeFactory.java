699063a6dbf792a6dd7621e132342d832be403bf
Neema Nejad <37365315+sharkcooper@users.noreply.github.com>
2021-04-29 14:11:51 -0700

    implemented two methods

--- src/CfpAlternativeFactory.java
+++ src/CfpAlternativeFactory.java
@@ -1,5 +1,12 @@
 import com.rma.factories.AbstractNewObjectFactory;
 import com.rma.factories.NewObjectFactory;
+import com.rma.io.FileManager;
+import com.rma.io.FileManagerImpl;
+import com.rma.io.RmaFile;
+import com.rma.model.Project;
+import com.rma.ui.GenericNewObjectPanel;
+import com.rma.ui.ProjectDialog;
+import sun.net.www.content.text.Generic;
 
 import javax.swing.*;
 
@@ -13,7 +20,16 @@ public class CfpAlternativeFactory extends AbstractNewObjectFactory implements N
 
     @Override
     public JComponent createNewObjectPanel() {
-        return null;
+        GenericNewObjectPanel panel = new GenericNewObjectPanel();
+        panel.setFileComponentsVisible(false);
+        Project p = Project.getCurrentProject();
+        panel.setName("");
+        panel.setDescription("");
+        panel.setExistingNamesList(_plugin.getAlternativeList());
+        panel.setDirectory(p.getProjectDirectory() +
+                RmaFile.separator +
+                _plugin.getPluginDirectory());
+        return panel;
     }
 
     @Override
@@ -23,7 +39,19 @@ public class CfpAlternativeFactory extends AbstractNewObjectFactory implements N
 
     @Override
     public Object createObject(JComponent jComponent) {
-        return null;
+        GenericNewObjectPanel panel = (GenericNewObjectPanel)jComponent;
+        CfpAlternative alt = new CfpAlternative();
+        alt.setName(panel.getSelectedName());
+        alt.setDescription(panel.getSelectedDescription());
+        alt.setFile(FileManagerImpl.getFileManager().getFile(panel.getSelectedFile().getPath() +
+                RmaFile.separator +
+                alt.getName() +
+                _plugin.getAltFileExtension()));
+        alt.setProject(Project.getCurrentProject());
+        _plugin.addAlternative(alt);
+        _plugin.editAlternative(alt);
+        alt.saveData();
+        return alt;
     }
 
     @Override
